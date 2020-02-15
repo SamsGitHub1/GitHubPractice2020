4b1eb74de7fbd662e628ba6794c2088ed1521e6b
SamsGitHub1 <44011188+SamsGitHub1@users.noreply.github.com>
2020-02-15 14:35:35 +0530

    additionmethodaddedby-Sam

--- src/Class3.java
+++ src/Class3.java
@@ -1,14 +1,16 @@
-
-public class Class3 {
-
-	
-		// TODO Auto-generated method stub
-	//Raj has done
-	//Sam added this line
-	//Sam added  new line by raj this line
-	//raj is working on this class
-
-
+public class Class3 
+{
+	public static void main(String args[])
+	{
+		System.out.println("This is sample Main");
 	}
+	
+	public static int addition(int a , int b)
+	{
+		int c=a+b;
+		return c;
+	}
+	
+}
 	
 
