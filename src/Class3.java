0bd6b843098932d41bd265b68ebaf901ff5e8614
rajtuduru <60816641+rajtuduru@users.noreply.github.com>
2020-02-15 14:39:06 +0530

    Sub method added by raj in class3

--- src/Class3.java
+++ src/Class3.java
@@ -4,13 +4,19 @@ public class Class3
 	{
 		System.out.println("This is sample Main");
 	}
-	
+	//add method by sam
 	public static int addition(int a , int b)
 	{
 		int c=a+b;
 		return c;
 	}
-	
-}
+	//sub method by raj
+	public static int sub(int a, int b)
+	{
+		int c= b-a;
+		return c;
+	}
+	}
+
 	
 
